8f9bc4fb30c7cdb0bb01a958c97512499d9afa3e
Damian Maclennan <damian@damianm.com>
2026-05-02 14:41:02 +1000

    add nats docs

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -33,6 +33,7 @@ export const docsNavigation = [
       { title: 'Azure Service Bus', slug: 'transports/azure-service-bus' },
       { title: 'Redis', slug: 'transports/redis' },
       { title: 'AMQP', slug: 'transports/amqp' },
+      { title: 'NATS', slug: 'transports/nats' },
       { title: 'SQL Server', slug: 'transports/sql-server' },
       { title: 'PostgreSQL', slug: 'transports/postgres' },
       { title: 'In-Process', slug: 'transports/in-process' },
